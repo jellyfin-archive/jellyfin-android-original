c59465d0641a1c6150133cbf186b74d56006ef6f
Luke <luke.pulverenti@gmail.com>
2015-05-01 16:41:23 -0400

    add device plugin

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -4,12 +4,20 @@ module.exports = [
         "file": "plugins/cordova-plugin-whitelist/whitelist.js",
         "id": "cordova-plugin-whitelist.whitelist",
         "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "clobbers": [
+            "device"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.0.1-dev"
+    "cordova-plugin-whitelist": "1.0.1-dev",
+    "cordova-plugin-device": "1.0.0"
 }
 // BOTTOM OF METADATA
 });
