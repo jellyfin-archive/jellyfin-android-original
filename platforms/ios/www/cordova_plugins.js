611e5214ce2199953c8e9833f3ab05a870af3a80
Luke <luke.pulverenti@gmail.com>
2015-05-01 16:41:23 -0400

    add device plugin
    Former-commit-id: c59465d0641a1c6150133cbf186b74d56006ef6f

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,9 +1,18 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "clobbers": [
+            "device"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.0.1-dev"
+    "cordova-plugin-whitelist": "1.0.1-dev",
+    "cordova-plugin-device": "1.0.0"
 }
 // BOTTOM OF METADATA
 });
