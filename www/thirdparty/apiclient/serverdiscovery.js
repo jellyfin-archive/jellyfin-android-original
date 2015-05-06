86486ef5fdc82ec47e101597bb87ee3e0bf092e1
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-05-06 01:37:02 -0400

    work on server discovery
    Former-commit-id: e008c8a3a20b0dd0b6cb1cfd24ddd4e07fbc07ce

--- www/thirdparty/apiclient/serverdiscovery.js
+++ www/thirdparty/apiclient/serverdiscovery.js
@@ -29,19 +29,54 @@
             }
 
             var data = stringToArrayBuffer('who is EmbyServer?');
+            var isTimedOut = false;
+            var socketId;
+
+            var timeout = setTimeout(function () {
+
+                isTimedOut = true;
+                deferred.resolveWith(null, [servers]);
+
+                if (socketId) {
+                    chrome.sockets.udp.onReceive.removeListener(onReceive);
+                    chrome.sockets.udp.close(socketId);
+                }
+
+            }, timeoutMs);
+
+            function onReceive(info) {
+                
+                if (info.socketId == socketId) {
+                    
+                    //expect(info.remotePort).toBeTruthy();
+                    //expect(info.remoteAddress).toBeTruthy();
+                    //expect(info).toBeValidUdpReadResultEqualTo(data);
+                }
+            }
 
             var port = 7359;
             chrome.sockets.udp.create(function (createInfo) {
+
+                socketId = createInfo.socketId;
+
                 chrome.sockets.udp.bind(createInfo.socketId, '0.0.0.0', port, function (result) {
                     chrome.sockets.udp.send(createInfo.socketId, data, '255.255.255.255', port, function (result) {
                         if (result < 0) {
                             console.log('send fail: ' + result);
                             chrome.sockets.udp.close(createInfo.socketId);
-                            deferred.resolveWith(null, [servers]);
+
+                            if (!isTimedOut) {
+                                clearTimeout(timeout);
+                                deferred.resolveWith(null, [servers]);
+                            }
+
                         } else {
+
                             console.log('sendTo: success ' + port);
-                            chrome.sockets.udp.close(createInfo.socketId);
-                            deferred.resolveWith(null, [servers]);
+
+                            if (!isTimedOut) {
+                                chrome.sockets.udp.onReceive.addListener(onReceive);
+                            }
                         }
                     });
                 });
