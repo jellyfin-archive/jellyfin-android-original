3764c21f8b8a83747de581ac0eb71ba0fabebd7c
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-05-15 09:15:39 -0400

    update scripts
    Former-commit-id: b3de171594b608316135f1f370b0dec9d8e90b02

--- www/scripts/connectlogin.js
+++ www/scripts/connectlogin.js
@@ -16,7 +16,7 @@
 
         }).fail(function () {
 
-            Dashboard.hideLoadingMsg();
+            Dashboard.hideModalLoadingMsg();
 
             Dashboard.alert({
                 message: Globalize.translate('MessageInvalidUser'),
@@ -31,7 +31,7 @@
 
     function handleConnectionResult(page, result) {
 
-        Dashboard.hideLoadingMsg();
+        Dashboard.hideModalLoadingMsg();
 
         switch (result.State) {
 
