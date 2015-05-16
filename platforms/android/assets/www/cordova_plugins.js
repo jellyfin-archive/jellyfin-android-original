4397102578c8a06f9a0e64f9650714df918eedda
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-05-16 16:09:30 -0400

    first android build
    Former-commit-id: 5856c8c54ab748421bad5e7b7ba23dac19a8ecdb

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,9 +1,37 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
-        "id": "cordova-plugin-whitelist.whitelist",
-        "runs": true
+        "file": "plugins/com.rd11.remote-controls/www/RemoteControls.js",
+        "id": "com.rd11.remote-controls.RemoteControls",
+        "clobbers": [
+            "window.remoteControls"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-chrome-apps-common/events.js",
+        "id": "cordova-plugin-chrome-apps-common.events",
+        "clobbers": [
+            "chrome.Event"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-chrome-apps-common/errors.js",
+        "id": "cordova-plugin-chrome-apps-common.errors"
+    },
+    {
+        "file": "plugins/cordova-plugin-chrome-apps-common/stubs.js",
+        "id": "cordova-plugin-chrome-apps-common.stubs"
+    },
+    {
+        "file": "plugins/cordova-plugin-chrome-apps-common/helpers.js",
+        "id": "cordova-plugin-chrome-apps-common.helpers"
+    },
+    {
+        "file": "plugins/cordova-plugin-chrome-apps-sockets-udp/sockets.udp.js",
+        "id": "cordova-plugin-chrome-apps-sockets-udp.sockets.udp",
+        "clobbers": [
+            "chrome.sockets.udp"
+        ]
     },
     {
         "file": "plugins/cordova-plugin-device/www/device.js",
@@ -34,18 +62,9 @@ module.exports = [
         ]
     },
     {
-        "file": "plugins/cordova-plugin-chrome-apps-sockets-udp/sockets.udp.js",
-        "id": "cordova-plugin-chrome-apps-sockets-udp.sockets.udp",
-        "clobbers": [
-            "chrome.sockets.udp"
-        ]
-    },
-    {
-        "file": "plugins/com.rd11.remote-controls/www/RemoteControls.js",
-        "id": "com.rd11.remote-controls.RemoteControls",
-        "clobbers": [
-            "window.remoteControls"
-        ]
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
     },
     {
         "file": "plugins/hu.dpal.phonegap.plugins.UniqueDeviceID/www/uniqueid.js",
@@ -60,42 +79,24 @@ module.exports = [
         "clobbers": [
             "SQLitePlugin"
         ]
-    },
-    {
-        "file": "plugins/cordova-plugin-chrome-apps-common/events.js",
-        "id": "cordova-plugin-chrome-apps-common.events",
-        "clobbers": [
-            "chrome.Event"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-chrome-apps-common/errors.js",
-        "id": "cordova-plugin-chrome-apps-common.errors"
-    },
-    {
-        "file": "plugins/cordova-plugin-chrome-apps-common/stubs.js",
-        "id": "cordova-plugin-chrome-apps-common.stubs"
-    },
-    {
-        "file": "plugins/cordova-plugin-chrome-apps-common/helpers.js",
-        "id": "cordova-plugin-chrome-apps-common.helpers"
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.0.1-dev",
-    "cordova-plugin-device": "1.0.0",
-    "cordova-plugin-console": "1.0.0",
-    "cordova-plugin-dialogs": "1.0.0",
-    "cordova-plugin-statusbar": "1.0.0",
-    "cordova-plugin-chrome-apps-sockets-udp": "1.2.2",
-    "com.telerik.plugins.wkwebview": "0.3.7",
     "com.crewmeister.cordova-ios-backgroundcolor": "0.0.1",
     "com.rd11.remote-controls": "1.0.0",
+    "com.telerik.plugins.wkwebview": "0.3.7",
+    "cordova-plugin-chrome-apps-common": "1.0.7",
+    "cordova-plugin-chrome-apps-iossocketscommon": "1.0.2",
+    "cordova-plugin-chrome-apps-sockets-udp": "1.2.2",
+    "cordova-plugin-console": "1.0.0",
+    "cordova-plugin-device": "1.0.0",
+    "cordova-plugin-dialogs": "1.0.0",
+    "cordova-plugin-statusbar": "1.0.0",
+    "cordova-plugin-whitelist": "1.0.1-dev",
     "hu.dpal.phonegap.plugins.UniqueDeviceID": "1.2.0",
-    "io.litehelpers.cordova.sqlite": "0.7.7",
-    "cordova-plugin-chrome-apps-common": "1.0.7"
+    "io.litehelpers.cordova.sqlite": "0.7.7"
 }
 // BOTTOM OF METADATA
 });
