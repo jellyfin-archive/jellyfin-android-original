a0334084ce7801fd74d20798f9fad3d2fbbd91f1
Luke <luke.pulverenti@gmail.com>
2015-05-17 23:24:39 -0400

    update script
    Former-commit-id: 53e4b2a630208eb652dd2bd3f0d9a6c6efe6c207

--- platforms/ios/www/thirdparty/cordova/chromecast.js
+++ platforms/ios/www/thirdparty/cordova/chromecast.js
@@ -1,1 +1,31 @@
-﻿
+﻿ (function(){
+  
+
+  function onDeviceFound(){
+  
+  }
+  
+  function onDeviceLost(){
+  
+  }
+  
+  function initSdk() {
+  
+    discoveryManager.setPairingLevel(ConnectSDK.PairingLevel.OFF);
+    discoveryManager.setAirPlayServiceMode(ConnectSDK.AirPlayServiceMode.Media);
+  
+  discoveryManager.addListener('devicefound', onDeviceFound);
+  
+  discoveryManager.addListener('devicelost', onDeviceLost);
+  }
+  
+  
+  
+  document.addEventListener("deviceready", function () {
+                            
+                            initSdk();
+                            
+}, false);
+  
+  
+  })();
