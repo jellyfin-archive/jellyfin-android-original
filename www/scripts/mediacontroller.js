56911b5c73d54f92a9936e20e3024120cf6f62ef
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-05-19 11:17:13 -0400

    update scripts
    Former-commit-id: 758ac74e47160db514de406c580cea2ea84ce580

--- www/scripts/mediacontroller.js
+++ www/scripts/mediacontroller.js
@@ -457,6 +457,20 @@
 
             return bottomText ? topText + '<br/>' + bottomText : topText;
         };
+
+        self.showPlaybackInfoErrorMessage = function(errorCode) {
+
+            // This timeout is messy, but if jqm is in the act of hiding a popup, it will not show a new one
+            // If we're coming from the popup play menu, this will be a problem
+
+            setTimeout(function() {
+                Dashboard.alert({
+                    message: Globalize.translate('MessagePlaybackError' + errorCode),
+                    title: Globalize.translate('HeaderPlaybackError')
+                });
+            }, 300);
+
+        };
     }
 
     window.MediaController = new mediaController();
--- www/thirdparty/cordova/generaldevice.js
+++ www/thirdparty/cordova/generaldevice.js
@@ -3,12 +3,12 @@
     var currentPairingDeviceId;
     var currentDevice;
 
-    function chromecastPlayer() {
+    function connectPlayer() {
 
         var self = this;
 
         // MediaController needs this
-        self.name = PlayerName;
+        self.name = "ConnectSDK";
 
         self.getItemsForPlayback = function (query) {
 
@@ -49,22 +49,6 @@
             $(self).trigger("positionchange", [state]);
         });
 
-        var endpointInfo;
-        function getEndpointInfo() {
-
-            if (endpointInfo) {
-
-                var deferred = $.Deferred();
-                deferred.resolveWith(null, [endpointInfo]);
-                return deferred.promise();
-            }
-
-            return ApiClient.getJSON(ApiClient.getUrl('System/Endpoint')).done(function (info) {
-
-                endpointInfo = info;
-            });
-        }
-
         self.play = function (options) {
 
             Dashboard.getCurrentUser().done(function (user) {
@@ -103,24 +87,64 @@
                 return;
             }
 
-            getEndpointInfo().done(function (endpoint) {
-
-                if (endpoint.IsLocal || endpoint.IsInNetwork) {
-                    ApiClient.getSystemInfo().done(function (info) {
-
-                        playInternal(options.items, info.LocalAddress);
-                    });
-                } else {
-                    playInternal(options.items);
-                }
-            });
-
+            playInternal(options.items);
         };
 
         function playInternal(items, serverAddress) {
-            
-            var bitrateSetting = AppSettings.maxChromecastBitrate();
 
+            playItemInternal(items[0], null, serverAddress);
+
+        }
+
+        function playItemInternal(items, startPosition) {
+
+            if (item == null) {
+                throw new Error("item cannot be null");
+            }
+
+            if (item.MediaType !== 'Audio' && item.MediaType !== 'Video') {
+                throw new Error("Unrecognized media type");
+            }
+
+            if (item.IsPlaceHolder) {
+                MediaController.showPlaybackInfoErrorMessage('PlaceHolder');
+                return;
+            }
+
+            var deviceProfile = self.getDeviceProfile();
+
+            if (item.MediaType === "Video") {
+
+                Dashboard.showModalLoadingMsg();
+            }
+
+            //getPlaybackInfo(item.Id, deviceProfile, startPosition).done(function (playbackInfoResult) {
+
+            //    if (validatePlaybackInfoResult(playbackInfoResult)) {
+
+            //        var mediaSource = getOptimalMediaSource(item.MediaType, playbackInfoResult.MediaSources);
+
+            //        if (mediaSource) {
+
+            //            if (mediaSource.RequiresOpening) {
+
+            //                getLiveStream(item.Id, playbackInfoResult.PlaySessionId, deviceProfile, startPosition, mediaSource, null, null).done(function (openLiveStreamResult) {
+
+            //                    openLiveStreamResult.MediaSource.enableDirectPlay = supportsDirectPlay(openLiveStreamResult.MediaSource);
+
+            //                    playInternalPostMediaSourceSelection(item, openLiveStreamResult.MediaSource, startPosition, callback);
+            //                });
+
+            //            } else {
+            //                playInternalPostMediaSourceSelection(item, mediaSource, startPosition, callback);
+            //            }
+            //        } else {
+            //            Dashboard.hideModalLoadingMsg();
+            //            MediaController.showPlaybackInfoErrorMessage('NoCompatibleStream');
+            //        }
+            //    }
+
+            //});
         }
 
         self.unpause = function () {
@@ -196,6 +220,142 @@
             volumeControl.setMute(!volumeControl.getMute());
         };
 
+        self.getDeviceProfile = function () {
+
+            var qualityOption = self.getVideoQualityOptions().filter(function (q) {
+                return q.selected;
+            })[0];
+
+            var bitrateSetting = AppSettings.maxStreamingBitrate();
+
+            var profile = {};
+
+            profile.MaxStreamingBitrate = bitrateSetting;
+            profile.MaxStaticBitrate = 40000000;
+            profile.MusicStreamingTranscodingBitrate = Math.min(bitrateSetting, 192000);
+
+            profile.DirectPlayProfiles = [];
+            profile.DirectPlayProfiles.push({
+                Container: 'mp4,m4v',
+                Type: 'Video',
+                VideoCodec: 'h264',
+                AudioCodec: 'aac,mp3,ac3'
+            });
+            profile.DirectPlayProfiles.push({
+                Container: 'mov',
+                Type: 'Video'
+            });
+
+            profile.DirectPlayProfiles.push({
+                Container: 'mp3',
+                Type: 'Audio'
+            });
+
+            profile.DirectPlayProfiles.push({
+                Container: 'aac',
+                Type: 'Audio'
+            });
+
+            profile.TranscodingProfiles = [];
+            profile.TranscodingProfiles.push({
+                Container: 'mp3',
+                Type: 'Audio',
+                AudioCodec: 'mp3',
+                Context: 'Streaming',
+                Protocol: 'http'
+            });
+
+            if (self.canPlayHls()) {
+                profile.TranscodingProfiles.push({
+                    Container: 'ts',
+                    Type: 'Video',
+                    AudioCodec: 'aac',
+                    VideoCodec: 'h264',
+                    Context: 'Streaming',
+                    Protocol: 'hls'
+                });
+            }
+
+            profile.TranscodingProfiles.push({
+                Container: 'mp4',
+                Type: 'Video',
+                AudioCodec: 'aac',
+                VideoCodec: 'h264',
+                Context: 'Streaming',
+                Protocol: 'http'
+            });
+
+            profile.ContainerProfiles = [];
+
+            profile.CodecProfiles = [];
+            profile.CodecProfiles.push({
+                Type: 'Audio',
+                Conditions: [{
+                    Condition: 'LessThanEqual',
+                    Property: 'AudioChannels',
+                    Value: '2'
+                }]
+            });
+
+            profile.CodecProfiles.push({
+                Type: 'VideoAudio',
+                Codec: 'aac,mp3',
+                Conditions: [
+                    {
+                        Condition: 'LessThanEqual',
+                        Property: 'AudioChannels',
+                        Value: '6'
+                    }
+                ]
+            });
+
+            profile.CodecProfiles.push({
+                Type: 'Video',
+                Codec: 'h264',
+                Conditions: [
+                {
+                    Condition: 'NotEquals',
+                    Property: 'IsAnamorphic',
+                    Value: 'true',
+                    IsRequired: false
+                },
+                {
+                    Condition: 'EqualsAny',
+                    Property: 'VideoProfile',
+                    Value: 'high|main|baseline|constrained baseline'
+                },
+                {
+                    Condition: 'LessThanEqual',
+                    Property: 'VideoLevel',
+                    Value: '41'
+                },
+                {
+                    Condition: 'LessThanEqual',
+                    Property: 'Width',
+                    Value: qualityOption.maxWidth
+                }]
+            });
+
+            // Subtitle profiles
+            profile.SubtitleProfiles = [];
+            profile.ResponseProfiles = [];
+
+            profile.ResponseProfiles.push({
+                Type: 'Video',
+                Container: 'm4v',
+                MimeType: 'video/mp4'
+            });
+
+            //profile.ResponseProfiles.push({
+            //    Type: 'Video',
+            //    Container: 'mkv',
+            //    MimeType: 'video/webm'
+            //});
+
+            return profile;
+        };
+
+
         function getBaseTargetInfo() {
             var target = {};
 
@@ -223,14 +383,13 @@
             return target;
         }
 
-        function isValid(name) {
+        function isValid(device) {
 
-            name = (name || '').toLowerCase();
-            var validTokens = ['nexusplayer', 'chromecast', 'eurekadongle'];
+            var validTokens = ['AirPlay', 'Airplay', 'airplay'];
 
             return validTokens.filter(function (t) {
 
-                return name.replace(' ', '').indexOf(t) != -1;
+                return device.hasService(t);
 
             }).length > 0;
         }
@@ -241,7 +400,7 @@
 
             return manager.getDeviceList().filter(function (d) {
 
-                return isValid(d.getModelName()) || isValid(d.getFriendlyName());
+                return isValid(d);
 
             }).map(convertDeviceToTarget);
         };
@@ -317,7 +476,7 @@
         };
 
         function onDisconnected(device) {
-            
+
             if (currentDevice && device.getId() == currentDevice.getId()) {
                 MediaController.removeActiveTarget(device.getId());
             }
@@ -395,7 +554,7 @@
 
     function initSdk() {
 
-        MediaController.registerPlayer(new chromecastPlayer());
+        MediaController.registerPlayer(new connectPlayer());
     }
 
     initSdk();
