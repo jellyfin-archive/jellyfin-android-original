f47a4129fa71af7a9ac89cb212d6e005ecd33039
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-05-22 16:22:59 -0400

    update android build
    Former-commit-id: 4688d9f82d7d95e4d6fff1dc1204ebbf6e49e4fb

--- platforms/android/assets/www/scripts/registrationservices.js
+++ platforms/android/assets/www/scripts/registrationservices.js
@@ -1,16 +1,60 @@
-﻿window.RegistrationServices = {
+﻿(function () {
 
-    renderPluginInfo: function (page, pkg, pluginSecurityInfo) {
+    function validatePlayback(deferred) {
 
+        var platform = (device.platform || '').toLowerCase();
 
-    },
+        // Don't require validation on android
+        if (platform.indexOf('android') != -1) {
+            deferred.resolve();
+            return;
+        }
 
-    addRecurringFields: function (page, period) {
-
-    },
-
-    initSupporterForm: function (page) {
-
-        $('.recurringSubscriptionCancellationHelp', page).html('');
+        deferred.resolve();
     }
-};
+
+    function validateLiveTV(deferred) {
+
+        var platform = (device.platform || '').toLowerCase();
+
+        // Don't require validation if not android
+        if (platform.indexOf('android') == -1) {
+            deferred.resolve();
+            return;
+        }
+
+        deferred.resolve();
+    }
+
+    window.RegistrationServices = {
+
+        renderPluginInfo: function (page, pkg, pluginSecurityInfo) {
+
+
+        },
+
+        addRecurringFields: function (page, period) {
+
+        },
+
+        initSupporterForm: function (page) {
+
+            $('.recurringSubscriptionCancellationHelp', page).html('');
+        },
+
+        validateFeature: function (name) {
+            var deferred = DeferredBuilder.Deferred();
+
+            if (name == 'playback') {
+                validatePlayback();
+            } else if (name == 'livetv') {
+                validateLiveTV();
+            } else {
+                deferred.resolve();
+            }
+
+            return deferred.promise();
+        }
+    };
+
+})();
