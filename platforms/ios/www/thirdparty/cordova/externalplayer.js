6902e3537e9872fadd739c650980174678713d8c
Luke <luke.pulverenti@gmail.com>
2015-05-25 02:20:33 -0400

    build 68
    Former-commit-id: c522182fc47c5be55999a549b1c74ebb160ee049

---
+++ platforms/ios/www/thirdparty/cordova/externalplayer.js
@@ -0,0 +1,22 @@
+﻿(function () {
+
+    function showPlayerSelectionMenu(item, url) {
+
+        window.plugins.launcher.launch({
+            packageName: 'com.mxtech.videoplayer.ad',
+            uri: url,
+            dataType: 'application/x-mpegURL'
+
+        }, function () {
+
+            console.log('plugin launch success');
+
+        }, function () {
+
+            console.log('plugin launch error');
+        });
+    }
+
+    window.ExternalPlayer.showPlayerSelectionMenu = showPlayerSelectionMenu;
+
+})();
