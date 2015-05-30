9946f41a1d36ad7ff346eef939b7013f4679814a
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-05-30 19:19:16 -0400

    update android
    Former-commit-id: 2dfb7b9b89bd1c92b2bc506b40c444cdcb9778f7

--- platforms/android/src/com/mb/android/io/NativeFileSystem.java
+++ platforms/android/src/com/mb/android/io/NativeFileSystem.java
@@ -1,12 +1,21 @@
 package com.mb.android.io;
 
+import com.mb.android.smbstreamer.Streamer;
+
 import org.xwalk.core.JavascriptInterface;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import jcifs.smb.SmbException;
 import jcifs.smb.SmbFile;
+import jcifs.smb.SmbFileInputStream;
 import mediabrowser.model.logging.ILogger;
 
 /**
@@ -51,5 +60,76 @@ public class NativeFileSystem {
         }
 
         return false;
+    }
+
+    public long getFileLength(String path) throws IOException {
+
+        File file = new File(path);
+
+        if (file.exists()) {
+            return file.length();
+        }
+
+        if (path.startsWith("\\\\")){
+            // convert unc to smb
+            path = path.replace("\\", "/");
+            path = "smb:" + path;
+        }
+
+        SmbFile sFile = new SmbFile(path);
+
+        if (sFile.exists()) {
+            return sFile.length();
+        }
+
+        throw new FileNotFoundException(path);
+    }
+
+    public String getFileName(String path) throws IOException {
+
+        File file = new File(path);
+
+        if (file.exists()) {
+            return file.getName();
+        }
+
+        if (path.startsWith("\\\\")){
+            // convert unc to smb
+            path = path.replace("\\", "/");
+            path = "smb:" + path;
+        }
+
+        SmbFile sFile = new SmbFile(path);
+
+        if (sFile.exists()) {
+            return sFile.getName();
+        }
+
+        throw new FileNotFoundException(path);
+    }
+
+    public InputStream getFileInputStream(String path) throws IOException {
+
+        File file = new File(path);
+
+        if (file.exists()) {
+            return new FileInputStream(path);
+        }
+
+        if (path.startsWith("\\\\")){
+            // convert unc to smb
+            path = path.replace("\\", "/");
+            path = "smb:" + path;
+        }
+
+        return new SmbFileInputStream(path);
+    }
+
+    @JavascriptInterface
+    public String translateFilePath(String path) {
+
+        Streamer streamer = Streamer.getInstance(logger);
+
+        return streamer.getUrl(path);
     }
 }
