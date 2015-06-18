d2e4679b66532f38f66fef1e51c18929a4bec494
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-06-18 18:08:08 -0400

    update chromecast

--- platforms/android/assets/www/scripts/tvlatest.js
+++ platforms/android/assets/www/scripts/tvlatest.js
@@ -1,5 +1,4 @@
-﻿(function($,document){function getView(){if(AppInfo.hasLowImageBandwidth){return'ThumbCard';}
-return'Thumb';}
+﻿(function($,document){function getView(){return'Thumb';}
 $(document).on('pageshowready',"#tvNextUpPage",function(){var userId=Dashboard.getCurrentUserId();var parentId=LibraryMenu.getTopParentId();var page=this;var limit=30;if(AppInfo.hasLowImageBandwidth){limit=16;}
 var options={IncludeItemTypes:"Episode",Limit:limit,Fields:"PrimaryImageAspectRatio,SyncInfo",ParentId:parentId,ImageTypeLimit:1,EnableImageTypes:"Primary,Backdrop,Banner,Thumb"};ApiClient.getJSON(ApiClient.getUrl('Users/'+userId+'/Items/Latest',options)).done(function(items){var view=getView();var html='';if(view=='ThumbCard'){html+=LibraryBrowser.getPosterViewHtml({items:items,shape:"backdrop",preferThumb:true,inheritThumb:false,showUnplayedIndicator:false,showChildCountIndicator:true,overlayText:false,showParentTitle:true,lazy:true,showTitle:true,cardLayout:true});}else if(view=='Thumb'){html+=LibraryBrowser.getPosterViewHtml({items:items,shape:"backdrop",preferThumb:true,inheritThumb:false,showParentTitle:false,showUnplayedIndicator:false,showChildCountIndicator:true,overlayText:false,centerText:true,lazy:true,showTitle:false});}
 $('#latestEpisodes',page).html(html).lazyChildren();});});})(jQuery,document);
