372886fb0569a3d7d7ed16776c78db332b69a2a3
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-06-19 02:32:30 -0400

    update components
    Former-commit-id: d90d1dc577208c452185e62eafb511e2654db1c4

---
+++ www/thirdparty/cordova/ios/actionsheet.js
@@ -0,0 +1,37 @@
+﻿(function () {
+
+    function onClosed() {
+        $(this).remove();
+    }
+    function show(options) {
+
+        // items
+        // positionTo
+        // showCancel
+        // title
+
+        var innerOptions = {
+            'title': options.title,
+            'buttonLabels': options.items.map(function (i) {
+                return i.name;
+            })
+        };
+
+        if (options.showCancel) {
+            innerOptions.addCancelButtonWithLabel = Globalize.translate('ButtonCancel');
+        }
+
+        // Depending on the buttonIndex, you can now call shareViaFacebook or shareViaTwitter
+        // of the SocialSharing plugin (https://github.com/EddyVerbruggen/SocialSharing-PhoneGap-Plugin)
+        window.plugins.actionsheet.show(innerOptions, function (index) {
+
+            if (options.callback) {
+                options.callback(options.items[index - 1].id);
+            }
+        });
+    }
+
+    window.ActionSheetElement = {
+        show: show
+    };
+})();
