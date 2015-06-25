92310171840e215b7244b88f645df1328db9b678
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-06-25 02:13:27 -0400

    update components
    Former-commit-id: 0817d0d79530db7f3a74662999b6fae9058a0ffe

--- www/scripts/appsettings.js
+++ www/scripts/appsettings.js
@@ -1,4 +1,4 @@
-﻿(function(window,appStorage){function update(key,val){appStorage.setItem(key,val);Events.trigger(AppSettings,'settingupdated',[key]);}
+﻿(function(window){function update(key,val){appStorage.setItem(key,val);Events.trigger(AppSettings,'settingupdated',[key]);}
 window.AppSettings={maxStreamingBitrate:function(val){if(val!=null){update('preferredVideoBitrate',val);}
 return parseInt(appStorage.getItem('preferredVideoBitrate')||'')||1500000;},maxChromecastBitrate:function(val){if(val!=null){update('chromecastBitrate',val);}
 return parseInt(appStorage.getItem('chromecastBitrate')||'')||3000000;},enableChromecastAc3:function(val){if(val!=null){update('enablechromecastac3',val.toString());}
