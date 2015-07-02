c59114a79350105f13078295e85c1d6f2cf58504
Luke <luke.pulverenti@gmail.com>
2015-07-02 08:58:01 -0400

    ios 146

--- platforms/ios/www/scripts/sharingmanager.js
+++ platforms/ios/www/scripts/sharingmanager.js
@@ -1,4 +1,4 @@
-﻿(function(){function onSharingSuccess(options){console.log('share success. shareId: '+options.share.Id);}
-function onSharingCancel(options){var shareId=options.share.Id;console.log('share cancelled. shareId: '+shareId);ApiClient.ajax({type:'DELETE',url:ApiClient.getUrl('Social/Shares/'+shareId)});}
+﻿(function(){function onSharingSuccess(options){Logger.log('share success. shareId: '+options.share.Id);}
+function onSharingCancel(options){var shareId=options.share.Id;Logger.log('share cancelled. shareId: '+shareId);ApiClient.ajax({type:'DELETE',url:ApiClient.getUrl('Social/Shares/'+shareId)});}
 function showMenu(userId,itemId){Dashboard.showLoadingMsg();require(['sharingwidget'],function(){ApiClient.ajax({type:'POST',url:ApiClient.getUrl('Social/Shares',{ItemId:itemId,UserId:userId}),dataType:"json"}).done(function(share){var options={share:share};Dashboard.hideLoadingMsg();SharingWidget.showMenu(options,onSharingSuccess,onSharingCancel);}).fail(function(){Dashboard.hideLoadingMsg();});});}
 window.SharingManager={showMenu:showMenu};})();
