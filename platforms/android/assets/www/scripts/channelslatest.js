e64897c10036787d3ce094c72f378fefb693d531
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-07-18 15:36:24 -0400

    update components

--- platforms/android/assets/www/scripts/channelslatest.js
+++ platforms/android/assets/www/scripts/channelslatest.js
@@ -1,2 +1,3 @@
-﻿(function($,document){function reloadItems(page){Sections.loadLatestChannelItems(page.querySelector('.items'),Dashboard.getCurrentUserId());}
-$(document).on('pagebeforeshowready',"#channelsLatestPage",function(){reloadItems(this);});})(jQuery,document);
+﻿(function($,document){function reloadItems(page){Sections.loadLatestChannelItems(page.querySelector('.latestItems'),Dashboard.getCurrentUserId());}
+function loadTab(page,index){switch(index){case 0:reloadItems(page);break;default:break;}}
+$(document).on('pageinitdepends',"#channelsPage",function(){var page=this;var pages=page.querySelector('neon-animated-pages');$(pages).on('tabchange',function(){loadTab(page,parseInt(this.selected));});});})(jQuery,document);
