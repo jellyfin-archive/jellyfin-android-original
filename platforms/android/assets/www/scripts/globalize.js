cfa07400d8249b9ee5878bf119abcfe971b2f73d
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-07-29 13:53:21 -0400

    update components

---
+++ platforms/android/assets/www/scripts/globalize.js
@@ -0,0 +1,16 @@
+﻿(function(){var dictionaries={};function getUrl(name,culture){var parts=culture.split('-');if(parts.length==2){parts[1]=parts[1].toUpperCase();culture=parts.join('-');}
+return'strings/'+name+'/'+culture+'.json';}
+function getDictionary(name,culture){return dictionaries[getUrl(name,culture)];}
+function loadDictionary(name,culture){var deferred=DeferredBuilder.Deferred();if(getDictionary(name,culture)){deferred.resolve();}else{var url=getUrl(name,culture);$.getJSON(url).done(function(dictionary){dictionaries[url]=dictionary;deferred.resolve();}).fail(function(){$.getJSON(getUrl(name,'en-US')).done(function(dictionary){dictionaries[url]=dictionary;deferred.resolve();});});}
+return deferred.promise();}
+var currentCulture='en-US';function setCulture(value){currentCulture=value;return $.when(loadDictionary('html',value),loadDictionary('javascript',value));}
+function getDeviceCulture(){var deferred=DeferredBuilder.Deferred();var culture;if(navigator.globalization&&navigator.globalization.getLocaleName){navigator.globalization.getLocaleName(function(locale){culture=(locale.value||'').replace('_','-');Logger.log('Device culture is '+culture);deferred.resolveWith(null,[culture]);},function(){deferred.resolveWith(null,[null]);});}else{culture=document.documentElement.getAttribute('data-culture');deferred.resolveWith(null,[culture]);}
+return deferred.promise();}
+function ensure(){var deferred=DeferredBuilder.Deferred();getDeviceCulture().done(function(culture){if(!culture){culture='en-US';}
+setCulture(culture).done(function(){deferred.resolve();});});return deferred.promise();}
+function translateDocument(html,dictionaryName){var glossary=getDictionary(dictionaryName,currentCulture)||{};return translateHtml(html,glossary);}
+function translateHtml(html,dictionary){var startIndex=html.indexOf('${');if(startIndex==-1){return html;}
+startIndex+=2;var endIndex=html.indexOf('}',startIndex);if(endIndex==-1){return html;}
+var key=html.substring(startIndex,endIndex);var val=dictionary[key]||key;html=html.replace('${'+key+'}',val);return translateHtml(html,dictionary);}
+window.Globalize={translate:function(key){var glossary=getDictionary('javascript',currentCulture)||{};var val=glossary[key]||key;for(var i=1;i<arguments.length;i++){val=val.replace('{'+(i-1)+'}',arguments[i]);}
+return val;},ensure:ensure,translateDocument:translateDocument};})();
