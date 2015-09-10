e7c1e70f9e8d95f99a3bab45dbadef269fdf0932
Luke <luke.pulverenti@gmail.com>
2015-09-10 14:32:53 -0400

    ios 230
    Former-commit-id: 564c3712545b05f8de47e55bce682b138d85cb29

--- platforms/ios/www/apiclient/localassetmanager.js
+++ platforms/ios/www/apiclient/localassetmanager.js
@@ -1,2 +1,4 @@
 ﻿(function(){function getLocalMediaSource(serverId,itemId){return null;}
-window.LocalAssetManager={getLocalMediaSource:getLocalMediaSource};})();
+function saveOfflineUser(user){var deferred=DeferredBuilder.Deferred();deferred.resolve();return deferred.promise();}
+function getCameraPhotos(){var deferred=DeferredBuilder.Deferred();deferred.resolveWith(null,[[]]);return deferred.promise();}
+window.LocalAssetManager={getLocalMediaSource:getLocalMediaSource,saveOfflineUser:saveOfflineUser,getCameraPhotos:getCameraPhotos};})();
