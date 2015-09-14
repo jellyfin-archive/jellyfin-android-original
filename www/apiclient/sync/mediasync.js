754a676bc6f7eb185c41bfe54308ad2d01e2234f
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-09-13 21:32:25 -0400

    update components

--- www/apiclient/sync/mediasync.js
+++ www/apiclient/sync/mediasync.js
@@ -1,3 +1,6 @@
-﻿(function(globalScope){function mediaSync(){var self=this;self.sync=function(apiClient){var deferred=DeferredBuilder.Deferred();deferred.resolve();return deferred.promise();};}
+﻿(function(globalScope){function mediaSync(){var self=this;self.sync=function(apiClient){var deferred=DeferredBuilder.Deferred();reportOfflineActions(apiClient).done(function(){syncData(apiClient,false).done(function(){getNewMedia(apiClient).done(function(){syncData(apiClient,false).done(function(){deferred.resolve();}).fail(getOnFail(deferred));}).fail(getOnFail(deferred));}).fail(getOnFail(deferred));}).fail(getOnFail(deferred));return deferred.promise();};function reportOfflineActions(apiClient){var deferred=DeferredBuilder.Deferred();deferred.resolve();return deferred.promise();}
+function syncData(apiClient,syncUserItemAccess){var deferred=DeferredBuilder.Deferred();deferred.resolve();return deferred.promise();}
+function getNewMedia(apiClient){var deferred=DeferredBuilder.Deferred();deferred.resolve();return deferred.promise();}
+function getOnFail(deferred){return function(){deferred.reject();};}}
 if(!globalScope.MediaBrowser){globalScope.MediaBrowser={};}
 globalScope.MediaBrowser.MediaSync=mediaSync;})(this);
