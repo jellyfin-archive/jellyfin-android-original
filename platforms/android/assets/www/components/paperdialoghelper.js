47635924a3057081d6df33efe32a0b85f004a9cd
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-09-17 13:39:36 -0400

    update components
    Former-commit-id: 9c9845ff87f9015ccf9c246f7b4f92a1435639ca

---
+++ platforms/android/assets/www/components/paperdialoghelper.js
@@ -0,0 +1,58 @@
+﻿(function (globalScope) {
+
+    function paperDialogHashHandler(dlg, hash) {
+
+        var isActive = true;
+
+        function onHashChange(e, data) {
+
+            data = data.state;
+            isActive = data.hash == '#' + hash;
+
+            if (data.direction == 'back') {
+                if (dlg) {
+                    if (!isActive) {
+                        dlg.close();
+                        dlg = null;
+                    }
+                }
+            }
+
+            if (isActive) {
+                document.body.classList.add('bodyWithPopupOpen');
+            }
+            else {
+                document.body.classList.remove('bodyWithPopupOpen');
+            }
+        }
+
+        function onDialogClosed() {
+
+            dlg = null;
+            $(window).off('navigate', onHashChange);
+
+            if (window.location.hash == '#' + hash) {
+                history.back();
+            }
+        }
+
+        var self = this;
+
+        $(dlg).on('iron-overlay-closed', onDialogClosed);
+        dlg.open();
+
+        window.location.hash = hash;
+
+        $(window).on('navigate', onHashChange);
+    }
+
+    function openWithHash(dlg, hash) {
+
+        new paperDialogHashHandler(dlg, hash);
+    }
+
+    globalScope.PaperDialogHelper = {
+        openWithHash: openWithHash
+    };
+
+})(this);
