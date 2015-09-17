94ca05011b0f94f6491c91b04397d51c5cab08e4
Luke <luke.pulverenti@gmail.com>
2015-09-17 14:32:01 -0400

    add background download plugin

--- platforms/ios/www/apiclient/fileupload.js
+++ platforms/ios/www/apiclient/fileupload.js
@@ -1,3 +1,3 @@
-﻿(function(globalScope){function fileUpload(){var self=this;self.upload=function(file,mimeType,name,url){var deferred=DeferredBuilder.Deferred();deferred.reject();return deferred.promise();};}
+﻿(function(globalScope){function fileUpload(){var self=this;self.upload=function(file,name,url){var deferred=DeferredBuilder.Deferred();deferred.reject();return deferred.promise();};}
 if(!globalScope.MediaBrowser){globalScope.MediaBrowser={};}
 globalScope.MediaBrowser.FileUpload=fileUpload;})(this);
