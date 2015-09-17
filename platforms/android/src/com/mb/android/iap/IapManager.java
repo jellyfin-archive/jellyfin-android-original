13274fd981615eeb1c00a0df4a5012398ab4304c
Eric Reed <ebr@mediabrowser3.com>
2015-09-17 15:50:16 -0400

    update IAP libs

--- platforms/android/src/com/mb/android/iap/IapManager.java
+++ platforms/android/src/com/mb/android/iap/IapManager.java
@@ -1,18 +1,23 @@
 package com.mb.android.iap;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.mb.android.webviews.IWebView;
 
 import org.xwalk.core.JavascriptInterface;
 
+import java.util.List;
+
 import mediabrowser.apiinteraction.EmptyResponse;
 import mediabrowser.apiinteraction.Response;
 import mediabrowser.model.logging.ILogger;
+import mediabrowser.model.serialization.IJsonSerializer;
 import tv.emby.iap.ErrorSeverity;
 import tv.emby.iap.ErrorType;
 import tv.emby.iap.IResultHandler;
 import tv.emby.iap.IabValidator;
+import tv.emby.iap.InAppProduct;
 import tv.emby.iap.ResultType;
 
 /**
@@ -23,6 +28,8 @@ public class IapManager {
     private IWebView webView;
     private ILogger logger;
     private Context context;
+    private IabValidator iabValidator;
+    private boolean operationInProgress;
 
     public final static String GOOGLE_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAk4MSP7wxlKaJwF066w7qQ+FvttXc+uSvUI5a+Lq+TT74Y1LTp0qg1+WRqou78WRK5cdfCr2m1N4LqttmYFfsWG/DBon98+ZFtaUbiP+Nx29YCkawE06hMyn0pONw/FnXB90mm0vGl7+fkpdYoUx1pit2DGoQweAZwmilW2jfPdi+YloSbX3SJlTXcgZIoAzIvY+qOinyuWIaRda5YcDfvson2yQC6XQOYqQ4ZOKhQxCSzaaQp3dLMCXlKPpsQNzFpVQsHLt4OntBMPkK3e/RxTE9AyhQYxofEzdKg/MHz1c3vCFIJCkzPy1cstwYMcjktRoLGgPHjxW60Iq9+USjfwIDAQAB";
 
@@ -43,12 +50,18 @@ public class IapManager {
     @JavascriptInterface
     public void isPurchased(final String id, final String callback) {
 
+        if (!confirmValidator()) {
+            logger.Error("*** Attempt to call isPurchased with IAP operation already in progress");
+            RespondToWebView(String.format("%s(\"%s\", %s)", callback, id, false));
+        }
+
         logger.Info("isPurchased: %s", id);
 
-        isPurchasedInternal("com.mb.android.unlock", new Response<Boolean>(){
+        operationInProgress = true;
+        isPurchasedInternal("com.mb.android.unlock", new Response<Boolean>() {
 
             @Override
-            public void onResponse(final Boolean result){
+            public void onResponse(final Boolean result) {
                 RespondToWebView(String.format("%s(\"%s\", %s)", callback, id, result));
             }
         });
@@ -61,21 +74,56 @@ public class IapManager {
     }
 
     private void isPurchasedInternal(String id, final Response<Boolean> response) {
-        final IabValidator iabValidator = new IabValidator(context, GOOGLE_KEY);
-
         iabValidator.checkInAppPurchase(id, new IResultHandler<ResultType>() {
             @Override
             public void onResult(ResultType resultType) {
-                response.onResponse(resultType.equals(ResultType.Success));
                 iabValidator.dispose();
+                operationInProgress = false;
+                logger.Info("*** IsPurchased Result: %s", resultType);
+                response.onResponse(resultType.equals(ResultType.Success));
             }
 
             @Override
             public void onError(ErrorSeverity errorSeverity, ErrorType errorType, String s) {
                 //TODO handle error...
+                logger.Info("*** IsPurchased Error %s", s);
                 iabValidator.dispose();
+                operationInProgress = false;
             }
         });
+
+    }
+
+    private boolean confirmValidator() {
+        if (operationInProgress) return false;
+        if (iabValidator == null || iabValidator.isDisposed()) iabValidator = new IabValidator(context, GOOGLE_KEY);
+        return true;
+    }
+
+    //Todo Test method - need to convert to method called from js
+    public void getAvailableProducts(final IJsonSerializer jsonSerializer) {
+        logger.Info("*** Get products test call");
+        if (!confirmValidator()) {
+            // an iap operation is currently in progress - only can have one at a time respond appropriately
+            logger.Error("*** Attempt to call get products with IAP operation already in progress");
+        } else {
+            iabValidator.getAvailableProductsAsync(new IResultHandler<List<InAppProduct>>() {
+                @Override
+                public void onResult(List<InAppProduct> inAppProducts) {
+                    logger.Info("*** In App product result");
+                    logger.Info(jsonSerializer.SerializeToString(inAppProducts));
+                    iabValidator.dispose();
+                }
+
+                @Override
+                public void onError(ErrorSeverity errorSeverity, ErrorType errorType, String s) {
+                    logger.Error("*** Error getting products: " + s);
+                    iabValidator.dispose();
+                }
+            });
+
+
+        }
 
     }
 }
