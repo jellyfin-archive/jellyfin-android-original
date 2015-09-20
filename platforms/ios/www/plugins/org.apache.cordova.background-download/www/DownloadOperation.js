5f9ae8c9f0ffaf41f65335035ddfe0e68df547f6
Luke <luke.pulverenti@gmail.com>
2015-09-20 15:54:52 -0400

    ios 247
    Former-commit-id: 4931ce97b0399a49fe3a527bbee9241548d881ba

--- platforms/ios/www/plugins/org.apache.cordova.background-download/www/DownloadOperation.js
+++ platforms/ios/www/plugins/org.apache.cordova.background-download/www/DownloadOperation.js
@@ -60,7 +60,7 @@ DownloadOperation.prototype.startAsync = function() {
             deferral.reject(err);
         };
 
-    exec(successCallback, errorCallback, "BackgroundDownload", "startAsync", [this.uri, this.resultFile.toURL()]);
+    exec(successCallback, errorCallback, "BackgroundDownload", "startAsync", [this.uri, this.resultFile]);
 
     // custom mechanism to trigger stop when user cancels pending operation
     deferral.promise.onCancelled = function () {
--- platforms/ios/www/scripts/syncactivity.js
+++ platforms/ios/www/scripts/syncactivity.js
@@ -1,6 +1,6 @@
 ﻿(function(){function cancelJob(page,id){var msg=Globalize.translate('CancelSyncJobConfirmation');Dashboard.confirm(msg,Globalize.translate('HeaderCancelSyncJob'),function(result){if(result){Dashboard.showLoadingMsg();ApiClient.ajax({url:ApiClient.getUrl('Sync/Jobs/'+id),type:'DELETE'}).done(function(){reloadData(page);});}});}
 function getSyncJobHtml(page,job,cardBoxCssClass,syncJobPage){var html='';html+="<div class='card squareCard' data-id='"+job.Id+"' data-status='"+job.Status+"'>";html+='<div class="'+cardBoxCssClass+'">';html+='<div class="cardScalable">';html+='<div class="cardPadder"></div>';syncJobPage+='?id='+job.Id;html+='<a class="cardContent" href="'+syncJobPage+'">';var imgUrl;var style='';if(job.PrimaryImageItemId){imgUrl=ApiClient.getScaledImageUrl(job.PrimaryImageItemId,{type:"Primary",width:400,tag:job.PrimaryImageTag});style="background-position:center center;";}else{style="background-color:#38c;background-position:center center;";imgUrl="css/images/items/detail/video.png";}
-html+='<div class="cardImage coveredCardImage lazy" data-src="'+imgUrl+'" style="'+style+'">';if(job.Progress&&job.Progress<100){html+='<div class="cardFooter">';html+="<div class='cardText cardProgress'>";html+='<progress class="itemProgressBar" min="0" max="100" value="'+job.Progress+'"></progress>';html+="</div>";html+="</div>";}
+html+='<div class="cardImage coveredCardImage lazy" data-src="'+imgUrl+'" style="'+style+'">';if(job.Progress&&job.Progress<100){html+='<div class="cardFooter fullCardFooter lightCardFooter">';html+="<div class='cardText cardProgress'>";html+='<progress class="itemProgressBar" min="0" max="100" value="'+job.Progress+'"></progress>';html+="</div>";html+="</div>";}
 html+="</div>";var opacity='.85';var background='rgba(204,51,51,'+opacity+')';var text=Globalize.translate('SyncJobStatus'+job.Status);if(job.Status=='Completed'){background='rgba(82, 181, 75, '+opacity+')';}
 else if(job.Status=='CompletedWithError'){}
 else if(job.Status=='Queued'){background='rgba(51, 136, 204, '+opacity+')';}
@@ -12,16 +12,18 @@ textLines.push(job.Name);if(job.ItemCount==1){textLines.push(Globalize.translate
 if(!job.ParentName){textLines.push('&nbsp;');}
 html+='<div class="cardText" style="text-align:right; float:right;padding:0;">';html+='<paper-icon-button icon="'+AppInfo.moreIcon+'" class="btnJobMenu"></paper-icon-button>';html+="</div>";for(var i=0,length=textLines.length;i<length;i++){html+="<div class='cardText' style='margin-right:30px;'>";html+=textLines[i];html+="</div>";}
 html+="</div>";html+="</div>";html+="</div>";return html;}
-function loadData(page,jobs){var html='';var lastTargetName='';var cardBoxCssClass='cardBox visualCardBox';var syncJobPage='syncjob.html';if($(page).hasClass('mySyncPage')){syncJobPage='mysyncjob.html';}
-for(var i=0,length=jobs.length;i<length;i++){var job=jobs[i];var targetName=job.TargetName||'Unknown';if(targetName!=lastTargetName){if(lastTargetName){html+='<br/>';html+='<br/>';html+='<br/>';}
-lastTargetName=targetName;html+='<div class="detailSectionHeader">';html+='<div style="display:inline-block;vertical-align:middle;">'+targetName+'</div>';html+='</div>';}
+function loadData(page,jobs){var html='';var lastTargetName='';var cardBoxCssClass='cardBox visualCardBox';var syncJobPage='syncjob.html';var showTargetName=true;if($(page).hasClass('mySyncPage')){syncJobPage='mysyncjob.html';showTargetName=!hasLocalSync();}
+for(var i=0,length=jobs.length;i<length;i++){var job=jobs[i];if(showTargetName){var targetName=job.TargetName||'Unknown';if(targetName!=lastTargetName){if(lastTargetName){html+='<br/>';html+='<br/>';html+='<br/>';}
+lastTargetName=targetName;html+='<div class="detailSectionHeader">';html+='<div>'+targetName+'</div>';html+='</div>';}}
 html+=getSyncJobHtml(page,job,cardBoxCssClass,syncJobPage);}
 var elem=$('.syncActivity',page).html(html).lazyChildren();Events.trigger(elem[0],'create');$('.btnJobMenu',elem).on('click',function(){showJobMenu(page,this);});if(!jobs.length){elem.html('<div style="padding:1em .25em;">'+Globalize.translate('MessageNoSyncJobsFound')+'</div>');}}
 function showJobMenu(page,elem){var card=$(elem).parents('.card');var jobId=card.attr('data-id');var status=card.attr('data-status');var menuItems=[];if(status=='Cancelled'){menuItems.push({name:Globalize.translate('ButtonDelete'),id:'delete',ironIcon:'delete'});}else{menuItems.push({name:Globalize.translate('ButtonCancelSyncJob'),id:'cancel',ironIcon:'delete'});}
 require(['actionsheet'],function(){ActionSheetElement.show({items:menuItems,positionTo:elem,callback:function(id){switch(id){case'delete':cancelJob(page,jobId);break;case'cancel':cancelJob(page,jobId);break;default:break;}}});});}
-function reloadData(page){Dashboard.showLoadingMsg();var options={};Dashboard.getCurrentUser().done(function(user){if($(page).hasClass('mySyncPage')){options.UserId=Dashboard.getCurrentUserId();}
+function hasLocalSync(){return Dashboard.capabilities().SupportsSync;}
+function reloadData(page){Dashboard.showLoadingMsg();var options={};Dashboard.getCurrentUser().done(function(user){if($(page).hasClass('mySyncPage')){options.UserId=Dashboard.getCurrentUserId();if(hasLocalSync()){options.TargetId=ApiClient.deviceId();}}
 ApiClient.getJSON(ApiClient.getUrl('Sync/Jobs',options)).done(function(response){loadData(page,response.Items);Dashboard.hideLoadingMsg();});});}
-function onWebSocketMessage(e,msg){var page=$.mobile.activePage;if(msg.MessageType=="SyncJobs"){loadData(page,msg.Data);}}
+function onWebSocketMessage(e,msg){var page=$.mobile.activePage;if(msg.MessageType=="SyncJobs"){var data=msg.Data;if(hasLocalSync()){var targetId=ApiClient.deviceId();data=data.filter(function(j){return j.TargetId==targetId;});}
+loadData(page,data);}}
 function startListening(page){var startParams="0,1500";if($(page).hasClass('mySyncPage')){startParams+=","+Dashboard.getCurrentUserId();}
 if(ApiClient.isWebSocketOpen()){ApiClient.sendWebSocketMessage("SyncJobsStart",startParams);}}
 function stopListening(){if(ApiClient.isWebSocketOpen()){ApiClient.sendWebSocketMessage("SyncJobsStop","");}}
