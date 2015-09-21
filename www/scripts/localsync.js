b01004bbf8a9dee6d29693f3f4e09c464690f9ce
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-09-21 17:18:30 -0400

    update background sync

--- www/scripts/localsync.js
+++ www/scripts/localsync.js
@@ -1,3 +1,4 @@
-﻿(function(){var syncPromise;window.LocalSync={isSupported:function(){return AppInfo.isNativeApp;},sync:function(options){if(syncPromise){return syncPromise.promise();}
-var deferred=DeferredBuilder.Deferred();require(['multiserversync'],function(){syncPromise=new MediaBrowser.MultiServerSync(ConnectionManager).sync(options).done(function(){syncPromise=null;deferred.resolve();}).fail(function(){syncPromise=null;});});return deferred.promise();},getSyncStatus:function(){if(syncPromise!=null){return'Syncing';}
-return'Idle';}};})();
+﻿(function(){var syncPromise;var lastStart=0;window.LocalSync={isSupported:function(){return AppInfo.isNativeApp;},sync:function(options){if(syncPromise){return syncPromise.promise();}
+var deferred=DeferredBuilder.Deferred();require(['multiserversync'],function(){lastStart=new Date().getTime();syncPromise=new MediaBrowser.MultiServerSync(ConnectionManager).sync(options).done(function(){syncPromise=null;deferred.resolve();}).fail(function(){syncPromise=null;});});return deferred.promise();},getSyncStatus:function(){if(syncPromise!=null){return'Syncing';}
+return'Idle';}};var syncInterval=1800000;function restartInterval(){if(LocalSync.isSupported){setInterval(function(){},syncInterval);if(lastStart>0&&(now-lastStart)>=syncInterval){}}}
+Dashboard.ready(restartInterval);document.addEventListener("resume",restartInterval,false);})();
