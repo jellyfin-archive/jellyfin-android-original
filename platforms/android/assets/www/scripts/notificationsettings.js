fd8ece8d7ec4db681f54fd4540a051792ed269f1
Luke Pulverenti <luke.pulverenti@gmail.com>
2015-10-14 13:27:38 -0400

    update components

--- platforms/android/assets/www/scripts/notificationsettings.js
+++ platforms/android/assets/www/scripts/notificationsettings.js
@@ -2,5 +2,6 @@
 itemHtml+='<h1>';itemHtml+=i.Category;itemHtml+='</h1>';itemHtml+='<div class="paperList" style="margin-bottom:2em;">';}
 itemHtml+='<a class="clearLink" href="notificationsetting.html?type='+i.Type+'">';itemHtml+='<paper-icon-item>';if(i.Enabled){itemHtml+='<paper-fab class="listAvatar blue" icon="notifications-active" item-icon></paper-fab>';}
 else{itemHtml+='<paper-fab class="listAvatar" style="background-color:#999;" icon="notifications-off" item-icon></paper-fab>';}
-itemHtml+='<paper-item-body two-line>';itemHtml+='<div>'+i.Name+'</div>';itemHtml+='</paper-item-body>';itemHtml+='<paper-icon-button icon="mode-edit"></paper-icon-button>';itemHtml+='</paper-icon-item>';itemHtml+='</a>';return itemHtml;}).join('');html+='</div>';$('.notificationList',page).html(html).trigger('create');Dashboard.hideLoadingMsg();});}
+itemHtml+='<paper-item-body two-line>';itemHtml+='<div>'+i.Name+'</div>';itemHtml+='</paper-item-body>';itemHtml+='<paper-icon-button icon="mode-edit"></paper-icon-button>';itemHtml+='</paper-icon-item>';itemHtml+='</a>';return itemHtml;}).join('');if(list.length){html+='</div>';}
+$('.notificationList',page).html(html).trigger('create');Dashboard.hideLoadingMsg();});}
 $(document).on('pageshow',"#notificationSettingsPage",function(){var page=this;reload(page);});})(jQuery,window);
