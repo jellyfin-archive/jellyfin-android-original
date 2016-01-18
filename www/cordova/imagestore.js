b388f8d94a5fa95925f6204eb894cbbdc3412462
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-01-18 14:25:33 -0500

    update components

--- www/cordova/imagestore.js
+++ www/cordova/imagestore.js
@@ -1,4 +1,4 @@
-﻿(function () {
+﻿define(['cryptojs-md5'], function () {
 
     function setImageIntoElement(elem, url) {
 
@@ -14,142 +14,132 @@
     var fileSystem;
     function getFileSystem() {
 
+        if (fileSystem) {
+            return Promise.resolve(fileSystem);
+        }
+
         return new Promise(function (resolve, reject) {
 
-            if (fileSystem) {
+            requestFileSystem(PERSISTENT, 0, function (fs) {
+                fileSystem = fs;
                 resolve(fileSystem);
-            } else {
-                requestFileSystem(PERSISTENT, 0, function (fs) {
-                    fileSystem = fs;
-                    resolve(fileSystem);
-                });
-            }
+            });
         });
     }
 
-    function indexedDbBlobImageStore() {
+    function getCacheKey(url) {
 
-        var self = this;
+        // Try to strip off the domain to share the cache between local and remote connections
+        var index = url.indexOf('://');
 
-        function getCacheKey(url) {
+        if (index != -1) {
+            url = url.substring(index + 3);
 
-            // Try to strip off the domain to share the cache between local and remote connections
-            var index = url.indexOf('://');
+            index = url.indexOf('/');
 
             if (index != -1) {
-                url = url.substring(index + 3);
-
-                index = url.indexOf('/');
-
-                if (index != -1) {
-                    url = url.substring(index + 1);
-                }
-
+                url = url.substring(index + 1);
             }
 
-            return CryptoJS.MD5(url).toString();
         }
 
-        function normalizeReturnUrl(url) {
-            if (browserInfo.safari) {
+        return CryptoJS.MD5(url).toString();
+    }
 
-                // Use the embedded server for iOS8, and also if we don't know the iOS version, just to be safe
-                var index = url.indexOf('/Documents');
-                if (index != -1) {
-                    return url.substring(index);
-                }
-                else {
-                    return url.replace('file://', '');
-                }
+    function normalizeReturnUrl(url) {
+        if (browserInfo.safari) {
+
+            // Use the embedded server for iOS8, and also if we don't know the iOS version, just to be safe
+            var index = url.indexOf('/Documents');
+            if (index != -1) {
+                return url.substring(index);
             }
-            return url;
+            else {
+                return url.replace('file://', '');
+            }
+        }
+        return url;
+    }
+
+    function getImageUrl(originalUrl) {
+
+        if (browserInfo.android && originalUrl.indexOf('tag=') != -1) {
+            originalUrl += "&accept=webp";
         }
 
-        self.getImageUrl = function (originalUrl) {
+        return new Promise(function (resolve, reject) {
 
-            if (browserInfo.android && originalUrl.indexOf('tag=') != -1) {
-                originalUrl += "&accept=webp";
-            }
+            var key = getCacheKey(originalUrl);
 
-            return new Promise(function (resolve, reject) {
+            //Logger.log('getImageUrl:' + originalUrl);
 
-                var key = getCacheKey(originalUrl);
+            getFileSystem().then(function (fileSystem) {
+                var path = fileSystem.root.toURL() + "/emby/cache/" + key;
 
-                //Logger.log('getImageUrl:' + originalUrl);
+                resolveLocalFileSystemURL(path, function (fileEntry) {
+                    var localUrl = normalizeReturnUrl(fileEntry.toURL());
+                    //Logger.log('returning cached file: ' + localUrl);
+                    resolve(localUrl);
 
-                getFileSystem().then(function (fileSystem) {
-                    var path = fileSystem.root.toURL() + "/emby/cache/" + key;
+                }, function () {
 
-                    resolveLocalFileSystemURL(path, function (fileEntry) {
-                        var localUrl = normalizeReturnUrl(fileEntry.toURL());
-                        //Logger.log('returning cached file: ' + localUrl);
+                    //Logger.log('downloading: ' + originalUrl);
+                    var ft = new FileTransfer();
+                    ft.download(originalUrl, path, function (entry) {
+
+                        var localUrl = normalizeReturnUrl(entry.toURL());
+
+                        //Logger.log(localUrl);
                         resolve(localUrl);
-
-                    }, function () {
-
-                        //Logger.log('downloading: ' + originalUrl);
-                        var ft = new FileTransfer();
-                        ft.download(originalUrl, path, function (entry) {
-
-                            var localUrl = normalizeReturnUrl(entry.toURL());
-
-                            //Logger.log(localUrl);
-                            resolve(localUrl);
-                        });
                     });
                 });
             });
+        });
+    }
+
+    var imageIdIndex = 1;
+    function setImageWithSdWebImage(elem, url) {
+
+        var rect = elem.getBoundingClientRect();
+
+        var options = {
+            data: url,
+            index: imageIdIndex,
+            quality: 0,
+            scale: Math.round(rect.width) + 'x' + Math.round(rect.height),
+            downloadOptions: window.CollectionRepeatImageOptions.SDWebImageRetryFailed | window.CollectionRepeatImageOptions.SDWebImageLowPriority | window.CollectionRepeatImageOptions.SDWebImageAllowInvalidSSLCertificates
         };
 
-        self.setImageInto = function (elem, url) {
+        if (elem.classList.contains('coveredCardImage')) {
+            options.scale += '!';
+        }
 
-            function onFail() {
-                setImageIntoElement(elem, url);
-            }
+        imageIdIndex++;
+
+        window.CollectionRepeatImage.getImage(options, function (data) {
+            var dataUrl = 'data:image/jpeg;base64,' + data;
+            elem.style.backgroundImage = "url('" + dataUrl + "')";
+        });
+    }
+
+    new indexedDbBlobImageStore();
+
+    return {
+        loadImage: function (elem, url) {
 
             //if (browserInfo.safari) {
             //    setImageWithSdWebImage(elem, url);
             //    return;
             //}
 
-            self.getImageUrl(url).then(function (localUrl) {
+            return getImageUrl(url).then(function (localUrl) {
 
                 setImageIntoElement(elem, localUrl);
 
-            }, onFail);
-        };
-
-        var imageIdIndex = 1;
-
-        function setImageWithSdWebImage(elem, url) {
-
-            var rect = elem.getBoundingClientRect();
-
-            var options = {
-                data: url,
-                index: imageIdIndex,
-                quality: 0,
-                scale: Math.round(rect.width) + 'x' + Math.round(rect.height),
-                downloadOptions: window.CollectionRepeatImageOptions.SDWebImageRetryFailed | window.CollectionRepeatImageOptions.SDWebImageLowPriority | window.CollectionRepeatImageOptions.SDWebImageAllowInvalidSSLCertificates
-            };
-
-            if (elem.classList.contains('coveredCardImage')) {
-                options.scale += '!';
-            }
-
-            imageIdIndex++;
-
-            window.CollectionRepeatImage.getImage(options, function (data) {
-                var dataUrl = 'data:image/jpeg;base64,' + data;
-                elem.style.backgroundImage = "url('" + dataUrl + "')";
+            }, function () {
+                setImageIntoElement(elem, url);
             });
         }
+    };
 
-        window.ImageStore = self;
-    }
-
-    require(['cryptojs-md5'], function () {
-        new indexedDbBlobImageStore();
-    });
-
-})();
+});
