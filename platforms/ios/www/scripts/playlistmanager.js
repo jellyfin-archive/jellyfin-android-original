0f3c1af4e6353dd47059441f006b6247c27603c5
Luke <luke.pulverenti@gmail.com>
2016-02-19 23:18:41 -0500

    ios 1.1.1
    Former-commit-id: e4b7cd808de0689ce7b0694c793d934693e30698

--- platforms/ios/www/scripts/playlistmanager.js
+++ platforms/ios/www/scripts/playlistmanager.js
@@ -1,1 +1,1 @@
-!function(){window.PlaylistManager={showPanel:function(e){require(["playlisteditor"],function(n){(new n).show(e)})},supportsPlaylists:function(e){return"Program"==e.Type?!1:e.RunTimeTicks||e.IsFolder||"Genre"==e.Type||"MusicGenre"==e.Type||"MusicArtist"==e.Type}}}(jQuery,document);
+define([],function(){return{showPanel:function(e){require(["playlisteditor"],function(n){(new n).show(e)})},supportsPlaylists:function(e){return"Program"==e.Type?!1:e.RunTimeTicks||e.IsFolder||"Genre"==e.Type||"MusicGenre"==e.Type||"MusicArtist"==e.Type}}});
