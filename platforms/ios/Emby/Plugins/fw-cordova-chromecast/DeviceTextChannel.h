099bd29360e6cc2ccfdc2ee7ab2ff339f9d6f7ed
Luke <luke.pulverenti@gmail.com>
2016-04-01 14:12:26 -0400

    update chrome cast
    Former-commit-id: 59c7d93dad0e5ea330db7ea08e671cf2878cbde9

--- platforms/ios/Emby/Plugins/fw-cordova-chromecast/DeviceTextChannel.h
+++ platforms/ios/Emby/Plugins/fw-cordova-chromecast/DeviceTextChannel.h
@@ -1,12 +1,9 @@
 #import "DeviceTextChannel.h"
+#import <GoogleCast/GoogleCast.h>
 
 // [START custom-channel-1]
 // This custom channel class extends GCKCastChannel.
-@implementation DeviceTextChannel
-
-- (void)didReceiveTextMessage:(NSString*)message {
-  NSLog(@"received message: %@", message);
-}
+@interface DeviceTextChannel : GCKCastChannel
 
 @end
 // [END custom-channel-1]
