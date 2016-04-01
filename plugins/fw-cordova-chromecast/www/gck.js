2bcf3f7bda834bb8a9712c3aa5bf029aeeae4f08
Luke Pulverenti <luke.pulverenti@gmail.com>
2016-04-01 15:28:18 -0400

    update chromecast.js

--- plugins/fw-cordova-chromecast/www/gck.js
+++ plugins/fw-cordova-chromecast/www/gck.js
@@ -113,8 +113,11 @@ exports.launchApplication = function () {
         }
 
         function onFailed(e, error) {
-            unbind();
-            reject(error);
+
+            setTimeout(function() {
+                unbind();
+                reject(error);
+            }, 500);
         }
 
         $(t).on("applicationLaunched", onLaunched);
@@ -144,8 +147,10 @@ exports.joinApplication = function () {
         }
 
         function onFailed(e, error) {
-            unbind();
-            reject(error);
+            setTimeout(function () {
+                unbind();
+                reject(error);
+            }, 500);
         }
 
         $(t).on("applicationLaunched", onLaunched);
