219c1054a2ec4dd1662a207fb4d3d80e83049076
Luke Pulverenti <luke.pulverenti@gmail.com>
2017-01-14 02:21:55 -0500

    remove unused plugins

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -1,32 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/cordova-plugin-chrome-apps-common/events.js",
-        "id": "cordova-plugin-chrome-apps-common.events",
-        "clobbers": [
-            "chrome.Event"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-chrome-apps-common/errors.js",
-        "id": "cordova-plugin-chrome-apps-common.errors"
-    },
-    {
-        "file": "plugins/cordova-plugin-chrome-apps-common/stubs.js",
-        "id": "cordova-plugin-chrome-apps-common.stubs"
-    },
-    {
-        "file": "plugins/cordova-plugin-chrome-apps-common/helpers.js",
-        "id": "cordova-plugin-chrome-apps-common.helpers"
-    },
-    {
-        "file": "plugins/cordova-plugin-chrome-apps-sockets-udp/sockets.udp.js",
-        "id": "cordova-plugin-chrome-apps-sockets-udp.sockets.udp",
-        "clobbers": [
-            "chrome.sockets.udp"
-        ]
-    },
-    {
         "file": "plugins/acidhax.cordova.chromecast/chrome.cast.js",
         "id": "acidhax.cordova.chromecast.ChromecastApi",
         "clobbers": [
