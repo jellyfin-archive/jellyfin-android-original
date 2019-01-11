549ec291f3133e2bd8d09a42ac12e5c97ba35c43
dkanada <dkanada@users.noreply.github.com>
2019-01-11 23:48:41 +0900

    remove incorrect licenses for now
    Former-commit-id: c975a12d29eca3d4b1255ba973a11c139ef2206f

--- src/cordova/transfermanager.js
+++ src/cordova/transfermanager.js
@@ -1,23 +1,3 @@
-/**
- * src/cordova/transfermanager.js
- * Part of the Jellyfin project (https://jellyfin.media)
- *
- * All copyright belongs to the Jellyfin contributors; a full list can
- * be found in the file CONTRIBUTORS.md
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 2.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
- */
-
 // TODO: This is non-functional
 define([], function() {
     "use strict";
