756e24b27283824ba8445be087e884e2320e651f
Bill Thornton <billt2006@gmail.com>
2019-01-11 22:22:25 -0500

    Add license header to new files

--- src/cordova/filerepository.js
+++ src/cordova/filerepository.js
@@ -1,3 +1,31 @@
+/**
+ * src/cordova/filerepository.js
+ * Part of the Jellyfin project (https://jellyfin.media)
+ *
+ *    This Source Code Form is subject to the terms of the Mozilla Public
+ *    License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ *    You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ *    All copyright belongs to the Jellyfin contributors; a full list can
+ *    be found in the file CONTRIBUTORS.md
+ *
+ *    Alternatively, the contents of this file may be used under the terms
+ *    of the GNU General Public License Version 2 or later, as described below:
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 // TODO: This is non-functional
 define([], function() {
     "use strict";
