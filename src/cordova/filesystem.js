2fb00f11ebb6d6ac661f98f7def7a3209d8fc6f6
dkanada <dkanada@users.noreply.github.com>
2019-01-13 16:06:11 +0900

    remove some bindings to prepare for plugin creation

--- src/cordova/filesystem.js
+++ src/cordova/filesystem.js
@@ -3,12 +3,6 @@ define([], function () {
 
     return {
         fileExists: function (path) {
-            var exists = NativeFileSystem.fileExists(path);
-            console.log('fileExists: ' + exists + ' - path: ' + path);
-            if (exists) {
-                return Promise.resolve();
-            }
-
             return Promise.reject();
         },
         directoryExists: function (path) {
