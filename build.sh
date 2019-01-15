7b631c333a1d3aa45a0655d5a08547aacbf0f4da
Joshua Boniface <joshua@boniface.me>
2019-01-15 01:11:01 -0500

    Add commented out production/development flags

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ set -o errexit
 
 cd ${SOURCE_DIR}
 
+export NODE_ENV=production
+#export NODE_ENV=development
+
 npm cache verify
 npm install
 npx gulp
@@ -13,6 +16,7 @@ npx cordova prepare
 
 export ANDROID_HOME=${ANDROID_DIR}
 npx cordova build android --release
+#npx cordova build android --debug
 
 mkdir -p ${ARTIFACT_DIR}/apk
 mmv "${SOURCE_DIR}/platforms/android/build/outputs/apk/android-*.apk" "${ARTIFACT_DIR}/apk/jellyfin-android_#1.apk"
