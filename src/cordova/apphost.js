ec79ff262f503bbd8c816c1cd77f0553cddb9ede
dkanada <dkanada@users.noreply.github.com>
2019-01-21 04:09:54 +0900

    remove ac3 for now
    Former-commit-id: 0ee8244952e4b01955e08543b4ea0f0ef119d8d3

--- src/cordova/apphost.js
+++ src/cordova/apphost.js
@@ -29,9 +29,10 @@ define(['appStorage', 'browser'], function (appStorage, browser) {
                 });
 
                 profile.DirectPlayProfiles.push({
+                    // TODO investigate ac3 support
                     Container: "m4v,3gp,ts,mpegts,mov,xvid,vob,mkv,wmv,asf,ogm,ogv,m2v,avi,mpg,mpeg,mp4,webm,wtv",
                     Type: 'Video',
-                    AudioCodec: 'aac,aac_latm,mp2,mp3,ac3,wma,dca,dts,pcm,PCM_S16LE,PCM_S24LE,opus,flac'
+                    AudioCodec: 'aac,aac_latm,mp2,mp3,wma,dca,dts,pcm,PCM_S16LE,PCM_S24LE,opus,flac'
                 });
 
                 profile.CodecProfiles = profile.CodecProfiles.filter(function (i) {
