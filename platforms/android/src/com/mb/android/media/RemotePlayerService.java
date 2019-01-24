71fd7c37fc61fb5513f27896641bdcd73484c1b3
dkanada <dkanada@users.noreply.github.com>
2019-01-24 17:31:02 +0900

    clean up PlayerService and fix system media control issue
    Former-commit-id: b8bad9b914499424556e2a8ca0c4531eafc2dbe2

--- platforms/android/src/com/mb/android/media/RemotePlayerService.java
+++ platforms/android/src/com/mb/android/media/RemotePlayerService.java
@@ -33,10 +33,10 @@ import mediabrowser.model.logging.ILogger;
 
 public class RemotePlayerService extends Service {
 
-    private MediaSessionManager m_objMediaSessionManager;
-    private MediaSession m_objMediaSession;
-    private MediaController m_objMediaController;
-
+    private VolleyHttpClient httpClient;
+    private MediaController mediaController;
+    private MediaSessionManager mediaSessionManager;
+    private MediaSession mediaSession;
     private String mediaSessionMediaId = "";
     private Bitmap largeItemIcon;
 
@@ -45,51 +45,62 @@ public class RemotePlayerService extends Service {
         return null;
     }
 
-    private void handleIntent(Intent intent) {
-        if (intent == null || intent.getAction() == null)
-            return;
+    @Override
+    public boolean onUnbind(Intent intent) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            mediaSession.release();
+            onStopped();
+        }
 
+        return super.onUnbind(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        if (intent == null || intent.getAction() == null) return;
         String action = intent.getAction();
 
-        if (action.equalsIgnoreCase(Constants.ACTION_REPORT)) {
+        if (action.equals(Constants.ACTION_REPORT)) {
             notify(intent);
             return;
         }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            if (action.equalsIgnoreCase(Constants.ACTION_PLAY)) {
-                m_objMediaController.getTransportControls().play();
-            } else if (action.equalsIgnoreCase(Constants.ACTION_PAUSE)) {
-                m_objMediaController.getTransportControls().pause();
-            } else if (action.equalsIgnoreCase(Constants.ACTION_FAST_FORWARD)) {
-                m_objMediaController.getTransportControls().fastForward();
-            } else if (action.equalsIgnoreCase(Constants.ACTION_REWIND)) {
-                m_objMediaController.getTransportControls().rewind();
-            } else if (action.equalsIgnoreCase(Constants.ACTION_PREVIOUS)) {
-                m_objMediaController.getTransportControls().skipToPrevious();
-            } else if (action.equalsIgnoreCase(Constants.ACTION_NEXT)) {
-                m_objMediaController.getTransportControls().skipToNext();
-            } else if (action.equalsIgnoreCase(Constants.ACTION_STOP)) {
-                m_objMediaController.getTransportControls().stop();
+            switch (action) {
+                case Constants.ACTION_PLAY:
+                    mediaController.getTransportControls().play();
+                    break;
+                case Constants.ACTION_PAUSE:
+                    mediaController.getTransportControls().pause();
+                    break;
+                case Constants.ACTION_FAST_FORWARD:
+                    mediaController.getTransportControls().fastForward();
+                    break;
+                case Constants.ACTION_REWIND:
+                    mediaController.getTransportControls().rewind();
+                    break;
+                case Constants.ACTION_PREVIOUS:
+                    mediaController.getTransportControls().skipToPrevious();
+                    break;
+                case Constants.ACTION_NEXT:
+                    mediaController.getTransportControls().skipToNext();
+                    break;
+                case Constants.ACTION_STOP:
+                    mediaController.getTransportControls().stop();
+                    break;
             }
         }
     }
 
-    private VolleyHttpClient httpClient;
-
     private void notify(final Intent handledIntent) {
-
         String playerAction = handledIntent.getStringExtra("playerAction");
-
-        if (playerAction.equalsIgnoreCase("playbackstop")) {
+        if (playerAction.equals("playbackstop")) {
             onStopped();
             return;
         }
 
         String itemId = handledIntent.getStringExtra("itemId");
         String imageUrl = handledIntent.getStringExtra("imageUrl");
-
-        if (largeItemIcon != null && mediaSessionMediaId.equalsIgnoreCase(itemId)) {
+        if (largeItemIcon != null && mediaSessionMediaId.equals(itemId)) {
             notifyWithBitmap(handledIntent, largeItemIcon);
             return;
         }
@@ -135,9 +146,8 @@ public class RemotePlayerService extends Service {
         boolean canSeek = handledIntent.getBooleanExtra("canSeek", false);
         boolean isLocalPlayer = handledIntent.getBooleanExtra("isLocalPlayer", false);
 
-        if (!mediaSessionMediaId.equalsIgnoreCase(itemId)) {
-            setMediaSessionMetadata(m_objMediaSession, itemId, artist, album, title, largeIcon);
-
+        if (!mediaSessionMediaId.equals(itemId)) {
+            setMediaSessionMetadata(mediaSession, itemId, artist, album, title, largeIcon);
             mediaSessionMediaId = itemId;
         }
 
@@ -150,7 +160,7 @@ public class RemotePlayerService extends Service {
                          generateAction(android.R.drawable.ic_media_pause, "Pause", Constants.ACTION_PAUSE);
 
             Notification.MediaStyle style = new Notification.MediaStyle();
-            style.setMediaSession(m_objMediaSession.getSessionToken());
+            style.setMediaSession(mediaSession.getSessionToken());
 
             Intent intent = new Intent(getApplicationContext(), RemotePlayerService.class);
             intent.setAction(Constants.ACTION_STOP);
@@ -166,7 +176,7 @@ public class RemotePlayerService extends Service {
                 stateBuilder.setState(PlaybackState.STATE_PLAYING, position, 1.0f);
             }
 
-            m_objMediaSession.setPlaybackState(stateBuilder.build());
+            mediaSession.setPlaybackState(stateBuilder.build());
 
             PendingIntent pendingIntent = PendingIntent.getService(getApplicationContext(), 1, intent, 0);
             Notification.Builder builder = new Notification.Builder(this)
@@ -196,24 +206,20 @@ public class RemotePlayerService extends Service {
             builder.addAction(generateAction( android.R.drawable.ic_media_ff, "Fast Forward", Constants.ACTION_FAST_FORWARD));
             builder.addAction(generateAction(android.R.drawable.ic_media_next, "Next", Constants.ACTION_NEXT));
 
-            //final TransportControls controls = m_objMediaSession.getController().getTransportControls();
-            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
             try {
+                NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
                 notificationManager.notify(1, builder.build());
             } catch (Exception e) {
                 e.printStackTrace();
-                // this will throw if the bitmap fails to load
             }
         }
     }
 
     private PendingIntent createContentIntent() {
-
-        Intent openUI = new Intent(this, MainActivity.class);
-        openUI.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        openUI.setAction(Constants.ACTION_SHOW_PLAYER);
-        return PendingIntent.getActivity(this, 100, openUI, PendingIntent.FLAG_CANCEL_CURRENT);
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        intent.setAction(Constants.ACTION_SHOW_PLAYER);
+        return PendingIntent.getActivity(this, 100, intent, PendingIntent.FLAG_CANCEL_CURRENT);
     }
 
     private void setMediaSessionMetadata(MediaSession mediaSession, String itemId, String artist, String album, String title, Bitmap largeIcon) {
@@ -306,88 +312,79 @@ public class RemotePlayerService extends Service {
 
     @Override
     public int onStartCommand(final Intent intent, int flags, int startId) {
-
-        if (m_objMediaSessionManager == null) {
+        if (mediaSessionManager == null) {
             initMediaSessions();
         }
 
         handleIntent(intent);
-
         return super.onStartCommand(intent, flags, startId);
     }
 
-    private void responseToWebView(String js) {
-        MainActivity.RespondToWebView(js);
-    }
-
     private void initMediaSessions() {
-
         mediaSessionMediaId = "";
         largeItemIcon = null;
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            mediaSessionManager = (MediaSessionManager) getSystemService(Context.MEDIA_SESSION_SERVICE);
+            mediaSession = new MediaSession(getApplicationContext(), "sample session");
+            mediaController = new MediaController(getApplicationContext(), mediaSession.getSessionToken());
+            mediaSession.setActive(true);
+            int flags = MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS | MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS;
+            mediaSession.setFlags(flags);
 
-            m_objMediaSessionManager = (MediaSessionManager) getSystemService(Context.MEDIA_SESSION_SERVICE);
-            m_objMediaSession = new MediaSession(getApplicationContext(), "sample session");
-            m_objMediaController = new MediaController(getApplicationContext(), m_objMediaSession.getSessionToken());
-            m_objMediaSession.setActive(true);
-            m_objMediaSession.setFlags(MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS | MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS);
-
-            m_objMediaSession.setCallback(new Callback() {
+            mediaSession.setCallback(new Callback() {
                 @Override
                 public void onPlay() {
                     super.onPlay();
-                    Log.e(Constants.LOG_TAG, "onPlay");
-                    //buildNotification( generateAction( android.R.drawable.ic_media_pause, "Pause", Constants.ACTION_PAUSE ) );
+                    Log.i(Constants.LOG_TAG, "onPlay");
                     MainActivity.sendCommand("playpause");
                 }
 
                 @Override
                 public void onPause() {
                     super.onPause();
-                    Log.e(Constants.LOG_TAG, "onPause");
-                    //buildNotification(generateAction(android.R.drawable.ic_media_play, "Play", Constants.ACTION_PLAY));
+                    Log.i(Constants.LOG_TAG, "onPause");
                     MainActivity.sendCommand("playpause");
                 }
 
                 @Override
                 public void onSkipToNext() {
                     super.onSkipToNext();
-                    Log.e(Constants.LOG_TAG, "onSkipToNext");
-                    //buildNotification( generateAction( android.R.drawable.ic_media_pause, "Pause", Constants.ACTION_PAUSE ) );
+                    Log.i(Constants.LOG_TAG, "onSkipToNext");
                     MainActivity.sendCommand("next");
                 }
 
                 @Override
                 public void onSkipToPrevious() {
                     super.onSkipToPrevious();
-                    Log.e(Constants.LOG_TAG, "onSkipToPrevious");
-                    //buildNotification( generateAction( android.R.drawable.ic_media_pause, "Pause", Constants.ACTION_PAUSE ) );
+                    Log.i(Constants.LOG_TAG, "onSkipToPrevious");
                     MainActivity.sendCommand("previous");
                 }
 
                 @Override
                 public void onFastForward() {
                     super.onFastForward();
+                    Log.i(Constants.LOG_TAG, "onFastForward");
                     MainActivity.sendCommand("fastforward");
                 }
 
                 @Override
                 public void onRewind() {
                     super.onRewind();
+                    Log.i(Constants.LOG_TAG, "onRewind");
                     MainActivity.sendCommand("rewind");
                 }
 
                 @Override
                 public void onStop() {
                     super.onStop();
+                    Log.i(Constants.LOG_TAG, "onStop");
                     MainActivity.sendCommand("stop");
                     onStopped();
                 }
 
                 @Override
                 public void onSeekTo(long pos) {
-
                     super.onSeekTo(pos);
                 }
 
@@ -397,15 +394,5 @@ public class RemotePlayerService extends Service {
                 }
             });
         }
-    }
-
-    @Override
-    public boolean onUnbind(Intent intent) {
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            m_objMediaSession.release();
-        }
-
-        return super.onUnbind(intent);
     }
 }
