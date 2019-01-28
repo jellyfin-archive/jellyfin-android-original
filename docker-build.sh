7256a067130096cf473c6a5eafa2b9f6b83502d4
Joshua Boniface <joshua@boniface.me>
2019-01-27 19:29:17 -0500

    Improve Dockerization of the build
    Former-commit-id: a4be8017be6d9f94599591f2e2ad82fcdfffbda0

--- docker-build.sh
+++ docker-build.sh
@@ -30,6 +30,10 @@ pushd src/jellyfin-web
 git fetch --all
 git checkout ${web_branch} || usage
 popd
+pushd src/cordova-plugin-chromecast
+git fetch --all
+git checkout master
+popd
 
 set -o xtrace
 package_temporary_dir="$( mktemp -d )"
@@ -37,8 +41,6 @@ current_user="$( whoami )"
 
 # Trap cleanup for latter sections
 cleanup() {
-    set +o errexit
-    docker image rm ${image_name} --force
     rm -rf "${package_temporary_dir}"
 }
 trap cleanup EXIT INT
@@ -46,7 +48,7 @@ trap cleanup EXIT INT
 # Set up the build environment docker image
 docker build . -t "${image_name}" -f ./${dockerfile}
 # Build the APKs and copy out to ${package_temporary_dir}
-docker run --rm -v "${package_temporary_dir}:/dist" "${image_name}"
+docker run --rm -v "${package_temporary_dir}:/dist" -v "$(pwd):/repo" "${image_name}"
 # Correct ownership on the APKs (as current user, then as root if that fails)
 chown -R "${current_user}" "${package_temporary_dir}" &>/dev/null \
   || sudo chown -R "${current_user}" "${package_temporary_dir}" &>/dev/null
