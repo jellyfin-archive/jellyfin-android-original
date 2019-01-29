20521ad6fdb9094b35bd779771de7fe7e66d0908
Joshua Boniface <joshua@boniface.me>
2019-01-29 00:01:59 -0500

    Clean up some options and organization

--- build.sh
+++ build.sh
@@ -10,23 +10,21 @@ cd ${SOURCE_DIR}
 # Trap cleanup for latter sections
 cleanup() {
     rm -rf node_modules/ platforms/ plugins/ www/
+    sed -i 's/src/file:src/g' package.json
 }
 trap cleanup EXIT INT
 
+export ANDROID_HOME=${ANDROID_DIR}
 export NODE_ENV=production
-#export NODE_ENV=development
 
-npm --version
 npm cache verify
-sed -i 's/src/file:src/g' package.json
 npm install
 sed -i 's/file:src/src/g' package.json
 npx gulp
+npx cordova platform add android
 npx cordova prepare
 
-export ANDROID_HOME=${ANDROID_DIR}
 npx cordova build android --release
-#npx cordova build android --debug
 
 mkdir -p ${ARTIFACT_DIR}/apk
 mmv "${SOURCE_DIR}/platforms/android/build/outputs/apk/android-*.apk" "${ARTIFACT_DIR}/apk/jellyfin-android_#1.apk"
