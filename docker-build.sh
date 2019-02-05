7e850f34be33a0d0bd272e8220f0052022bedbcd
Joshua Boniface <joshua@boniface.me>
2019-02-04 22:18:29 -0500

    Improve Docker build layout and naming
    Former-commit-id: 9bbf46f5cf519f6cc8a1cd408c98a5dfa4603502

--- docker-build.sh
+++ docker-build.sh
@@ -1,57 +1,29 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+# Builds the APK inside the Docker container
 
 set -o errexit
-
-dockerfile="Dockerfile"
-image_name="jellyfin-android-apkbuild"
-
-# Initialize the submodules
-git submodule update --init
-
-usage() {
-    echo -e "Usage:"
-    echo -e " $0 [--web/-w <branch>]"
-    echo -e "The web branch defaults to 'origin/master'; specify any valid tag or branch for the 'jellyfin-web' repository."
-    exit 1
-}
-
-# Check out the proper jellyfin-web branch
-if [[ ${1} == '--web' || ${1} == '-w' ]]; then
-    if [[ -n ${2} ]]; then
-        web_branch="${2}"
-    else
-        usage
-    fi
-else
-    web_branch="origin/master"
-fi
-
-pushd src/jellyfin-web
-git fetch --all
-git checkout ${web_branch} || usage
-popd
-pushd src/cordova-plugin-chromecast
-git fetch --all
-git checkout master
-popd
-
 set -o xtrace
-package_temporary_dir="$( mktemp -d )"
-current_user="$( whoami )"
 
-# Trap cleanup for latter sections
-cleanup() {
-    # Remove tempdir
-    rm -rf "${package_temporary_dir}"
-}
-trap cleanup EXIT INT
+# Move to source directory
+pushd ${SOURCE_DIR}
 
-# Set up the build environment docker image
-docker build . -t "${image_name}" -f ./${dockerfile}
-# Build the APKs and copy out to ${package_temporary_dir}
-docker run --rm -v "${package_temporary_dir}:/dist" "${image_name}"
-# Correct ownership on the APKs (as current user, then as root if that fails)
-chown -R "${current_user}" "${package_temporary_dir}" &>/dev/null \
-  || sudo chown -R "${current_user}" "${package_temporary_dir}" &>/dev/null
-# Move the APKs to the parent directory
-mv "${package_temporary_dir}"/apk/*.apk ../
+# Export environment variables
+export ANDROID_HOME=${ANDROID_DIR}
+export NODE_ENV=production
+
+# Install dependencies
+npm cache verify
+sed -i 's/"src/"file:src/g' package.json
+npm install
+sed -i 's/"file:src/"src/g' package.json
+npx gulp
+npx cordova platform rm android
+npx cordova platform add android
+
+# Build APK
+npx cordova build android --release
+
+# Move the artifacts out
+mkdir -p ${ARTIFACT_DIR}/apk
+mmv "${SOURCE_DIR}/platforms/android/build/outputs/apk/android-*.apk" "${ARTIFACT_DIR}/apk/jellyfin-android_#1.apk"
