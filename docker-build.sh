29f3a8ef3ffa12c2ad0162f3b8ff1d3818844bf6
Joshua Boniface <joshua@boniface.me>
2019-02-05 20:39:13 -0500

    Improve the build script allowing release spec

--- docker-build.sh
+++ docker-build.sh
@@ -5,12 +5,31 @@
 set -o errexit
 set -o xtrace
 
-# Move to source directory
-pushd ${SOURCE_DIR}
+# Handle the release type
+case ${RELEASE} in
+    'production')
+        RELEASE_SUFFIX=""
+        NODE_ENV="production"
+        RFLAG="--release"
+    ;;
+    'unminified')
+        RELEASE_SUFFIX="unminified_"
+        NODE_ENV="development"
+        RFLAG="--release"
+    ;;
+    'debug')
+        RELEASE_SUFFIX="debug_"
+        NODE_ENV="development"
+        RFLAG=""
+    ;;
+esac
 
 # Export environment variables
 export ANDROID_HOME=${ANDROID_DIR}
-export NODE_ENV=production
+export NODE_ENV
+
+# Move to source directory
+pushd ${SOURCE_DIR}
 
 # Install dependencies
 npm cache verify
@@ -22,8 +41,8 @@ npx cordova platform rm android
 npx cordova platform add android
 
 # Build APK
-npx cordova build android --release
+npx cordova build android ${RFLAG}
 
 # Move the artifacts out
 mkdir -p ${ARTIFACT_DIR}/apk
-mmv "${SOURCE_DIR}/platforms/android/build/outputs/apk/android-*.apk" "${ARTIFACT_DIR}/apk/jellyfin-android_#1.apk"
+mmv "${SOURCE_DIR}/platforms/android/build/outputs/apk/android-*.apk" "${ARTIFACT_DIR}/apk/jellyfin-android_${RELEASE_SUFFIX}#1.apk"
