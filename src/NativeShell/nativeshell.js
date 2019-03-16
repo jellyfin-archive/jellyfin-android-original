1c12ce68b6a4bb1733f0d44bd61f5b02da3bf438
vitorsemeano <vitorsemeano@gmail.com>
2019-03-16 19:30:32 +0000

    added sync method to nativeshell.js

--- src/NativeShell/nativeshell.js
+++ src/NativeShell/nativeshell.js
@@ -7,7 +7,7 @@ function invokeMethod(successCallback, errorCallback, method, options) {
 }
 
 function NativeShell() {
-    
+
     this.FileSystem = {
         fileExists: function (path) {
             return Promise.reject();
@@ -20,27 +20,30 @@ function NativeShell() {
 
 NativeShell.prototype.getDeviceInformation = function(successCallback, errorCallback) {
     invokeMethod(successCallback, errorCallback, 'getDeviceInformation', []);
-}
+};
 
 NativeShell.prototype.enableFullscreen = function(successCallback, errorCallback) {
     invokeMethod(successCallback, errorCallback, 'enableFullscreen', []);
-}
+};
 
 NativeShell.prototype.disableFullscreen = function(successCallback, errorCallback) {
     invokeMethod(successCallback, errorCallback, 'disableFullscreen', []);
-}
+};
 
 NativeShell.prototype.updateMediaSession = function(successCallback, errorCallback, options) {
     invokeMethod(successCallback, errorCallback, 'updateMediaSession', [options]);
-}
+};
 
 NativeShell.prototype.hideMediaSession = function(successCallback, errorCallback) {
     invokeMethod(successCallback, errorCallback, 'hideMediaSession', []);
-}
+};
 
 NativeShell.prototype.openUrl = function (url, target) {
     cordova.InAppBrowser.open(url, target || '_system');
 };
 
-var nativeShell = new NativeShell();
-module.exports = nativeShell;
+NativeShell.prototype.sync = function(successCallback, errorCallback) {
+    // TODO implement or remove
+};
+
+module.exports = new NativeShell();
